d53a582a685658ae206459d302a7c6a3eee58817
KalyanRajSahu-Snap <kalyanrajsahu@gmail.com>
2024-11-22 21:30:41 +0530

    Add root route

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get('/', (req, res) => {
+  res.send('Welcome to the School Management API!');
+});
+
 // Middleware to parse JSON
 app.use(express.json());
 
